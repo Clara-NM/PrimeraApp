a631587f0c86d49389603f83c253ab4141f9ac5c
Clara-NM <clara.nuevomota@gmail.com>
2024-09-19 09:59:29 +0200

    Primer subida de mi primera app

---
+++ SegundaApp/src/main/java/com/mycompany/segundaapp/SegundaApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.segundaapp;
+
+/**
+ *
+ * @author Clara
+ */
+public class SegundaApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
